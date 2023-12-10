7b7aa104114b7a23270c61bf161b308c5d8326b2
ayushvora01 <128839365+ayushvora01@users.noreply.github.com>
2023-12-10 19:49:56 +0530

    Update App.tsx

--- App.tsx
+++ App.tsx
@@ -5,8 +5,8 @@
  * @format
  */
 
-import React from 'react';
-import type { PropsWithChildren } from 'react';
+import React, { useEffect } from 'react';
+import { PropsWithChildren, useState } from 'react';
 import {
   SafeAreaView,
   ScrollView,
@@ -16,10 +16,13 @@ import {
   useColorScheme,
   View,
   Button,
+  FlatList,
+  SectionList,
+  TouchableHighlight,
+  TouchableOpacity,
+  ActivityIndicator,
 } from 'react-native';
 
-import UserData from './components/UserData';
-import CompanyData from './components/CompanyData';
 import {
   Colors,
   DebugInstructions,
@@ -32,39 +35,32 @@ type SectionProps = PropsWithChildren<{
   title: string;
 }>;
 
-// const name = "Ayush";
-// var email = "Ayush123@gmail.com"
-// let age = 17;
-// function University() {
-//   return "Marwadi University";  
-// }
-
-
-const test1 = (val: string) => {
-  console.warn(val);
-}
-const test2 = () => {
-  console.warn("Function Called");
-}
-
-let data = 30;
 
 function App(): JSX.Element {
   const isDarkMode = useColorScheme() === 'dark';
 
+  const [load,setLoad]=useState(false);
+  const displayLoader=()=>{
+    setLoad(true);
+    setTimeout(()=>{
+      setLoad(false);
+    },3000);
+  }
   return (
-    <View>
-      <Text style={{ fontSize: 40, fontWeight: "600", color: "purple" }}>Hello,Ayush Vora</Text>
-      {/* <Text style={{ fontSize: 50, fontWeight: "600", color: "purple" }}>Hello,{name} Vora</Text> */}
-      {/* <Text style={{ fontSize: 30, fontWeight: "600", color: "blue" }}>{University()}</Text> */}
-      {/* <Text style={{ fontSize: 30, fontWeight: "600", color: "blue" }}>{100 - 90}</Text> */}
-      {/* <Text style={{ fontSize: 30, fontWeight: "600", color: "blue" }}>{age===18?"Above 18":"Below 18"}</Text> */}
-      <UserData />
-      <CompanyData />
-      <Button title='Press Here' color={'green'} onPress={() => test1("Hello, Ayush Vora")}></Button>
-      <Button title='Press Here' color={'orange'} onPress={test2}></Button>
+    <View style={Styles.main}>
+     <ActivityIndicator size={100} color={'red'} animating={load}/>
+     <Button title='Load' onPress={displayLoader}/>
     </View>
   );
 }
+
+const Styles = StyleSheet.create({
+  main: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+
+})
 
 export default App;
